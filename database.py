27978c46ba9e98d9df16dd0668917718ce3e0626
Hanaoka Mizuki <mizuki260124@gmail.com>
2026-05-12 00:42:33 +0900

    一旦構造体の状態から商品情報を取り出す状態、databaseはテーブルを作るところからが何もかけてない。

--- database.py
+++ database.py
@@ -1,1 +1,36 @@
-print('確認用')
+import sqlite3
+import json
+import scraper
+
+#API_response.jsonとscrape_data.jsonファイルのデータをfanza_data.dbに保存する
+def save_purchased_data():
+
+    #API_valuesにAPI_responseの辞書型のデータを保存
+    API_values = []
+
+    #API_response.jsonファイルを読み込み
+    with open("API_response.json", "r", encoding="utf-8") as f:
+        API_response = json.load(f)    
+
+    #API_responseのデータが入っている構造を取得するため日付に対するvalueを代入
+    API_response_items = API_response["data"]["items"]
+
+    #API_responseの日付:データのリストin辞書を平坦化
+    for data in API_response_items.items():
+        #日付が0、商品情報が1に入ってる
+        purchase_date = data[0]
+        purchase_item = data[1][0]
+
+        #商品情報に日付情報を追加
+        purchase_item["data"] = purchase_date
+        API_values.append(purchase_item)
+
+    #データベースファイル作成
+    fanza_data = sqlite3.connect("fanza_data.db")
+
+    #データベースに接続
+    cur = fanza_data.cursor()
+
+    #テーブルに入るkeyをDBのテーブル形式で出力
+    
+save_purchased_data()
--- scraper.py
+++ scraper.py
@@ -7,8 +7,9 @@ from dotenv import load_dotenv
 
 MYPURCHASES_API_URL = "https://www.dmm.co.jp/dc/doujin/api/mylibraries/?page=1&sort=purchasedate_desc&genre=all&limit=20"
 
-
+#APIとスクレイピングでデータを取り出し、jsonファイル形式で保存する
 def fetch_purchased_data():
+
     #cookieをenvファイルから呼び出し
     load_dotenv()
     FANZA_COOKIE = {"cookie": os.getenv("FANZA_COOKIE")}
@@ -17,27 +18,12 @@ def fetch_purchased_data():
     API_MAX_PAGES = 1000
     API_max_items = 20
     HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
-    all_data = []
     
-    #requestsでAPIにアクセス、テキスト形式で取得
-    API_response = requests.get(url=MYPURCHASES_API_URL, headers=HEADERS, cookies=FANZA_COOKIE).text
+    #requestsでAPIにアクセスしてjson形式に変換
+    API_response = requests.get(url=MYPURCHASES_API_URL, headers=HEADERS, cookies=FANZA_COOKIE).json()
 
-    #[]の中身だけを取り出してリスト化
-    data_list = re.findall(r"\[(.*?)\]", API_response)
+    #API_response.jsonにAPIのデータを保存
+    with open("API_response.json", "w", encoding="utf-8") as f:
+        json.dump(API_response, f, ensure_ascii=False, indent=4)
 
-   
-    for i in data_list:
-        try:#リストの中身が{}で囲まれただけのstrならjsonを読み込む
-            all_data.append(json.loads(i))
-
-        except:#複数ある場合はもう一度{}で分割して同様にjson形式を読み込む
-            split_data = re.findall(r"\{.*?\}", i)
-            for k in split_data:
-                all_data.append(json.loads(k))
-
-    #all_dataには[{商品ごとの情報}, {...}, {...}]という形で書かれてる    
-    for purchases in all_data:
-        title = purchases["title"]
-        #これでタイトルだけを取り出す
-        print(title)
 fetch_purchased_data()
