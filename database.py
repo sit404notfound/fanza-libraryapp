dea4720e440e3e74b47ad8e5261cb31179cf5f75
hanazukimi <mizuki260124@gmail.com>
2026-05-18 23:32:00 +0900

    APIのデータをdbに入れるとこまで

--- database.py
+++ database.py
@@ -17,13 +17,14 @@ def save_purchased_data():
 
     #API_responseの日付:データのリストin辞書を平坦化
     for data in API_response_items.items():
-        #日付が0、商品情報が1に入ってる
-        purchase_date = data[0]
-        purchase_item = data[1][0]
+        for i in range(len(data[1])):
+            #日付が0、商品情報が1に入ってる
+            purchase_date = data[0]
+            purchase_item = data[1][i]
 
-        #商品情報に日付情報を追加
-        purchase_item["data"] = purchase_date
-        API_values.append(purchase_item)
+            #商品情報に日付情報を追加
+            purchase_item["data"] = purchase_date
+            API_values.append(purchase_item)
 
     #データベースファイル作成
     fanza_data = sqlite3.connect("fanza_data.db")
@@ -31,6 +32,61 @@ def save_purchased_data():
     #データベースに接続
     cur = fanza_data.cursor()
 
-    #テーブルに入るkeyをDBのテーブル形式で出力
-    
+    #ここからチャッピー
+    columns = []
+
+    for key, value in API_values[0].items():
+
+        if isinstance(value, bool):
+            sql_type = "INTEGER"
+
+        elif isinstance(value, int):
+            sql_type = "INTEGER"
+
+        else:
+            sql_type = "TEXT"
+
+        columns.append(f"{key} {sql_type}")
+
+    columns_sql = ", ".join(columns)
+
+    # -----------------------------
+    # テーブル作成
+    # -----------------------------
+
+    create_sql = f"""
+    CREATE TABLE IF NOT EXISTS products (
+        {columns_sql}
+    )
+    """
+
+    cur.execute(create_sql)
+
+    # -----------------------------
+    # INSERT文作成
+    # -----------------------------
+
+    column_names = ", ".join(API_values[0].keys())
+
+    placeholders = ", ".join(["?"] * len(API_values[0]))
+
+    insert_sql = f"""
+    INSERT INTO products ({column_names})
+    VALUES ({placeholders})
+    """
+
+    # -----------------------------
+    # データ追加
+    # -----------------------------
+
+    for data in API_values:
+
+        values = tuple(data.values())
+
+        cur.execute(insert_sql, values)
+
+    # 保存
+    fanza_data.commit()
+
+    fanza_data.close()
 save_purchased_data()
